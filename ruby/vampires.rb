ac75fd78aae21b07b787a94d41789013fd2a280d
kristaprokop <krista.prokop@gmail.com>
2016-08-02 17:26:58 -0500

    Revise flow and case statement; fix end of loop puts

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,53 +1,57 @@
-
-loops = 0
-allergies = ""
+employeesentered = 0   #set starting var for entries loop
 
 puts "How many employees do you have to enter today?"
 entries = gets.chomp
-entries = entries.to_i
+entries = entries.to_i  #convert answer to integer
 
-until loops == entries
-puts "What is your name?"
-name = gets.chomp
+until employeesentered == entries  #employeesentered var increases by 1 until it matches the number of entries per user input
+	puts "What is your name?"
+	name = gets.chomp
+	name = name.downcase #convert answer to lowercase to ensure string match
 
-puts "how old are you?"
-age = gets.chomp
-age = age.to_i
+	puts "how old are you?"
+	age = gets.chomp
+	age = age.to_i
 
-puts "What year were you born?"
-year = gets.chomp
-year = year.to_i
+	puts "What year were you born?"
+	year = gets.chomp
+	year = year.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n"
-garlic = gets.chomp
+agetrue = Time.new.year - age == year #calculates age accuracy and sets boolean for test
 
-puts "Would you like to enroll in the company’s health insurance? y/n"
-enroll = gets.chomp
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n"
+	garlic = gets.chomp
+	garlic = garlic.downcase
 
-until allergies == "done"
-	puts "If you have allergies, type one of them in and press enter. When you are finished type 'done'."
-	allergies = gets.chomp
-	if allergies == "sunshine"
-	break
-end
-end
+	puts "Would you like to enroll in the company’s health insurance? y/n"
+	enroll = gets.chomp
+	enroll = enroll.downcase
 
-agetrue = Time.new.year - age == year
-case 
-when (name =~ /Drake Cula/i) || (name =~ /Tu Fang/i) || (allergies =~ /sunlight/i)
-	puts "Definitely a vampire"
-when agetrue && (garlic == "y" || enroll == "y")
-	puts "Probably not a vampire"
-when (!agetrue && garlic == "n" && enroll == "n")
-	puts "Almost certainly a vampire"
-when !agetrue && (garlic == "n" || enroll == "n")
-	puts "Probably a vampire"
+allergies = ""  #sets allergies var to avoid skipping this question after one entry answers "done"
+	until allergies == "done"
+		puts "Type in your allergies one by one. Type 'done' when finished."
+		allergies = gets.chomp
+		allergies = allergies.downcase
+		if allergies == "sunshine"
+			break #exits allergies question and moves on to case
+		else  #in this case the empty else restarts the loop
+		end
+	end
 
-else
-	puts "Results inconclusive"
-end
+	puts case
+	when name == "drake cula" || name == "tu fang" || allergies == "sunshine"
+		 "Definitely a vampire"
+	when agetrue && (garlic == "y" || enroll == "y")
+		 "Probably not a vampire"
+	when (!agetrue && garlic == "n" && enroll == "n")
+		 "Almost certainly a vampire"
+	when !agetrue && (garlic == "n" || enroll == "n")
+		 "Probably a vampire"
+	else
+		 "Results inconclusive"
+	end
 
-loops = loops + 1
+employeesentered = employeesentered + 1
 end
 
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends." 
