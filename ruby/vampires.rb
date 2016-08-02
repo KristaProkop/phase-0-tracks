bce70afada4792609f9eb45b120d937eaaa9ce7c
kristaprokop <krista.prokop@gmail.com>
2016-08-01 21:15:45 -0500

    Added survey questionnaire logic for results

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -20,3 +20,15 @@ garlic = gets.chomp
 puts "Would you like to enroll in the company’s health insurance? y/n"
 enroll = gets.chomp
 
+case 
+when name == "Drake Cula" || "Tu Fang"
+ 	puts "Definitely a vampire."
+when 2016 - age == year && garlic == "y" || enroll == "y"
+ 	puts "Probably not a vampire."
+when 2016 - age != year && garlic == "n" || enroll == "n"
+ 	puts "Probably a vampire."
+when 2016 - age != year && garlic == "n" && enroll == "n"
+ 	puts "Almost certainly a vampire."
+else	
+ 	puts "Results inconclusive."
+end
