8e8fc523a122381812b5cc299e3a550e2c270bea
kristaprokop <krista.prokop@gmail.com>
2016-08-01 20:55:18 -0500

    Added initial enrollment questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,22 @@
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "how old are you?"
+age = gets.chomp
+age = age.to_i
+
+puts "What year were you born?"
+year = gets.chomp
+year = year.to_i
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? y/n"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance? y/n"
+enroll = gets.chomp
+
