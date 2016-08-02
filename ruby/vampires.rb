cb133e04ba8fb9a12ce73efa68ff8cbd2a868b09
kristaprokop <krista.prokop@gmail.com>
2016-08-01 22:18:27 -0500

    Add all boolean operators except Definitely A Vampire

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,7 +1,4 @@
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
+
 
 puts "What is your name?"
 name = gets.chomp
@@ -20,15 +17,20 @@ garlic = gets.chomp
 puts "Would you like to enroll in the company’s health insurance? y/n"
 enroll = gets.chomp
 
+agetrue = 2016 - age == year
+
 case 
-when name == "Drake Cula" || "Tu Fang"
- 	puts "Definitely a vampire."
-when 2016 - age == year && garlic == "y" || enroll == "y"
- 	puts "Probably not a vampire."
-when 2016 - age != year && garlic == "n" || enroll == "n"
- 	puts "Probably a vampire."
-when 2016 - age != year && garlic == "n" && enroll == "n"
- 	puts "Almost certainly a vampire."
-else	
- 	puts "Results inconclusive."
+
+when agetrue && (garlic == "y" || enroll == "y")
+	puts "Probably not a vampire"
+when (!agetrue && garlic == "n" && enroll == "n")
+	puts "Almost certainly a vampire"
+when !agetrue && (garlic == "n" || enroll == "n")
+	puts "Probably a vampire"
+# NAME MATCH ISNT WORKING when name == "Drake Cula", "Tu Fang"
+# 	puts "Definitely a vampire"
+else
+	puts "Results inconclusive"
 end
+
+
