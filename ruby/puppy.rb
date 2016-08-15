b796c7dd89ff95e132899f416c2bff0b0374e1f2
kristaprokop <krista.prokop@gmail.com>
2016-08-15 15:46:57 -0500

    Add puppy class characteristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#Puppy class
+
+# SPECIES ------------------------
+# Canis familiaris
+
+# CHARACTERISTICS ----------------
+# Breed: Varies
+# Size: Varies
+# Color: Varies
+
+# PERSONALITY -----------------------
+# Active
+# Friendly
+# Curious
