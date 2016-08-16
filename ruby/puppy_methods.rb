24b2bef7062b1e900e71e24003b2eb51ce8b29e1
kristaprokop <krista.prokop@gmail.com>
2016-08-16 13:06:22 -0500

    Add classes and methods from pairing

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,80 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+ def initialize
+   puts "initializing new puppy instance"
+ end
+
+ def fetch(toy)
+   puts "I brought back the #{toy}!"
+   toy
+ end
+
+ def speak(number)
+   number.times { puts 'woof'}
+   #puts "Woof!" * number
+ end
+
+ def roll_over
+   print "*rolls over*"
+ end
+
+ def dog_years(human_years)
+   dog_years = human_years * 7
+   return dog_years
+ end
+
+ def jump
+   print "*jumps into air*"
+ end
 
 end
 
+#Release 2
+class Soccer_player
+
+ def initialize(name)
+   puts "Behold a soccer player is born!"
+   @name = name
+ end
+
+ def score_a_goal(integer)
+   puts "#{@name} scored #{integer} goals"
+ end
+
+ def sprints(integer)
+   puts "#{@name} sprints #{integer} yards"
+
+ end
+
+
+end
+
+# DRIVER CODE
+
+boris = Puppy.new
+boris.fetch('tennis ball')
+boris.speak(3)
+boris.roll_over
+p boris.dog_years(1)
+boris.jump
+
+player = Soccer_player.new('Boris')
+player.score_a_goal(3)
+player.sprints(4)
+
+soccer_players = []
+example_names = [ 'krista', 'chris', 'ellen', 'pele', 'mary', 'jack' ]
+example_score_a_goal = [ 1, 2, 75 ]
+example_sprints = [ 2, 4, 100 ]
+
+50.times do |player1|
+  player1 = Soccer_player.new(example_names.sample)
+  soccer_players << player1
+ end
+
+soccer_players.each { |player|
+ player.sprints(example_sprints.sample)
+ player.score_a_goal(example_score_a_goal.sample)
+}
+
+p soccer_players
